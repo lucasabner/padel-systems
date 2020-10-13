afe6676fee7e108a52e37848fa0c651cd6bcf40f
Wagner Inácio <wagner.inacio16@outlook.com>
2020-10-12 21:00:32 -0300

    Refatoração da view Distribuir Jogos(ComboBox)
    Refatorações de smells: modularizando view em componentes (Long Method, Large Class). Extraindo Actions para controller; removendo imports inutilizados;

--- Coding/Desktop_and_Web/src/main/java/fourzeta/desktop_views/DistribuirJogos.java
+++ Coding/Desktop_and_Web/src/main/java/fourzeta/desktop_views/DistribuirJogos.java
@@ -21,35 +21,34 @@ public class DistribuirJogos extends JFrame {
 	private Torneio torneio;
 	private JLabel lblDistribirJogos;
 	private JLabel lblCategoria;
-	private JComboBox comboBoxCategoria;
-	private JComboBox comboBoxQuadra;
+	private JComboBox<String> comboBoxCategoria;
+	private JComboBox<String> comboBoxQuadra;
 	private DistribuirJogosController controller;
 	private JLabel lblQuadra;
 	private JButton btnVoltar;
 	private JButton btnDistribuir;
 
 	public DistribuirJogos(Usuario usuario, Torneio torneio) {
-		this.usuario = usuario;
-		this.torneio = torneio;
-		setTitle("Distribuir Jogos");
-		this.setSize(SIZE);
-		this.setLocationRelativeTo(null);
-		getContentPane().setLayout(null);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		configFrame();
+		this.getContentPane().add(configLblDistribuirJogos());
+		this.getContentPane().add(configLblCategoria());
+		this.getContentPane().add(configLblQuadra());
+		this.getContentPane().add(configBtnDistribuir());
+		this.getContentPane().add(configLblNotify());
+		this.getContentPane().add(configComboQuadra());
+		//this.getContentPane().add(configLblCategoria());
+	
+//		private JComboBox configComboCategoria() {
+//			comboBoxCategoria = new JComboBox();
+//			comboBoxCategoria.addItem("SELECIONAR");
+//			comboBoxCategoria.setBounds(22, 98, 300, 24);
+//			comboBoxCategoria.setFont(new Font("Times New Roman", Font.BOLD, 14));
+//			categoria[] catEn
+//
+//
+//return comboBoxCategoria;
+//		}
 
-		lblDistribirJogos = new JLabel("Distribuir Jogos");
-		lblDistribirJogos.setHorizontalAlignment(SwingConstants.CENTER);
-		lblDistribirJogos.setFont(new Font("Dialog", Font.BOLD, 30));
-		lblDistribirJogos.setBounds(12, 22, 321, 35);
-		getContentPane().add(lblDistribirJogos);
-
-		lblCategoria = new JLabel("Categoria:");
-		lblCategoria.setFont(new Font("Dialog", Font.BOLD, 14));
-		lblCategoria.setBounds(22, 75, 100, 15);
-		getContentPane().add(lblCategoria);
-
-		comboBoxCategoria = new JComboBox();
-		comboBoxCategoria.addItem("SELECIONAR");
 		comboBoxCategoria.addItem("PRIMEIRA");
 		comboBoxCategoria.addItem("SEGUNDA");
 		comboBoxCategoria.addItem("TERCEIRA");
@@ -57,25 +56,10 @@ public class DistribuirJogos extends JFrame {
 		comboBoxCategoria.addItem("QUINTA");
 		comboBoxCategoria.addItem("INICIANTES");
 		getContentPane().add(comboBoxCategoria);
-		comboBoxCategoria.setBounds(22, 98, 300, 24);
 		getContentPane().add(comboBoxCategoria);
 
-		lblQuadra = new JLabel("Quadra:");
-		lblQuadra.setFont(new Font("Dialog", Font.BOLD, 14));
-		lblQuadra.setBounds(22, 150, 70, 15);
-		getContentPane().add(lblQuadra);
-
-		comboBoxQuadra = new JComboBox();
-		comboBoxQuadra.addItem("SELECIONAR");	
-		comboBoxQuadra.addItem("LARANJA");
-		comboBoxQuadra.addItem("AZUL");
-		comboBoxQuadra.addItem("VERDE");
-
-		getContentPane().add(comboBoxQuadra);
-		comboBoxQuadra.setBounds(22, 173, 300, 24);
-		getContentPane().add(comboBoxQuadra);
-
 		
+	
 		btnVoltar = new JButton("Voltar");
 		btnVoltar.addActionListener(new ActionListener() {
 
@@ -95,127 +79,146 @@ public class DistribuirJogos extends JFrame {
 		btnVoltar.setBounds(22, 215, 114, 25);
 		getContentPane().add(btnVoltar);
 
+		
+
+		
+	}
+
+	private void configFrame() {
+		setTitle("Distribuir Jogos");
+		this.setSize(SIZE);
+		this.setLocationRelativeTo(null);
+		getContentPane().setLayout(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	}
+
+	private JLabel configLblDistribuirJogos() {
+		lblDistribirJogos = new JLabel("Distribuir Jogos");
+		lblDistribirJogos.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDistribirJogos.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		lblDistribirJogos.setBounds(12, 22, 321, 35);
+		getContentPane().add(lblDistribirJogos);
+		return lblDistribirJogos;
+	}
+
+	private JLabel configLblCategoria() {
+		lblCategoria = new JLabel("Categoria:");
+		lblCategoria.setFont(new Font("Times New Roman", Font.BOLD, 14));
+		lblCategoria.setBounds(22, 75, 100, 15);
+		getContentPane().add(lblCategoria);
+		return lblCategoria;
+	}
+
+	private JLabel configLblQuadra() {
+		lblQuadra = new JLabel("Quadra:");
+		lblQuadra.setFont(new Font("Times New Roman", Font.BOLD, 14));
+		lblQuadra.setBounds(22, 150, 70, 15);
+		getContentPane().add(lblQuadra);
+		return lblQuadra;
+
+	}
+
+	private JButton configBtnDistribuir() {
 		btnDistribuir = new JButton("Distribuir");
 		btnDistribuir.setBounds(208, 215, 114, 25);
-		controller = new DistribuirJogosController(usuario,torneio,this);
+		controller = new DistribuirJogosController(usuario, torneio, this);
 		btnDistribuir.addActionListener(controller);
-	
 		getContentPane().add(btnDistribuir);
-		
+		return btnDistribuir;
+	}
+
+	private JLabel configLblNotify() {
 		JLabel lblselecioneACategoria = new JLabel("*Distribuir jogos por categoria na quadra desejada");
 		lblselecioneACategoria.setFont(new Font("Dialog", Font.PLAIN, 12));
 		lblselecioneACategoria.setBounds(22, 249, 311, 43);
 		getContentPane().add(lblselecioneACategoria);
+		return lblselecioneACategoria;
 	}
-	
-	
+
+	private JComboBox configComboQuadra() {
+		comboBoxQuadra = new JComboBox();
+		comboBoxQuadra.addItem("SELECIONAR");
+		comboBoxQuadra.addItem("LARANJA");
+		comboBoxQuadra.addItem("AZUL");
+		comboBoxQuadra.addItem("VERDE");
+		getContentPane().add(comboBoxQuadra);
+		comboBoxQuadra.setBounds(22, 173, 300, 24);
+		getContentPane().add(comboBoxQuadra);
+		return comboBoxQuadra;
+	}
 
 	public Usuario getUsuario() {
 		return usuario;
 	}
 
-
-
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
-
-
 
 	public Torneio getTorneio() {
 		return torneio;
 	}
 
-
 	public void setTorneio(Torneio torneio) {
 		this.torneio = torneio;
 	}
-
-
 
 	public JLabel getLblDistribirJogos() {
 		return lblDistribirJogos;
 	}
 
-
-
 	public void setLblDistribirJogos(JLabel lblDistribirJogos) {
 		this.lblDistribirJogos = lblDistribirJogos;
 	}
-
-
 
 	public JLabel getLblCategoria() {
 		return lblCategoria;
 	}
 
-
-
 	public void setLblCategoria(JLabel lblCategoria) {
 		this.lblCategoria = lblCategoria;
 	}
-
-
 
 	public JComboBox getComboBoxCategoria() {
 		return comboBoxCategoria;
 	}
 
-
-
 	public void setComboBoxCategoria(JComboBox comboBoxCategoria) {
 		this.comboBoxCategoria = comboBoxCategoria;
 	}
-
-
 
 	public JComboBox getComboBoxQuadra() {
 		return comboBoxQuadra;
 	}
 
-
-
 	public void setComboBoxQuadra(JComboBox comboBoxQuadra) {
 		this.comboBoxQuadra = comboBoxQuadra;
 	}
-
-
 
 	public JLabel getLblQuadra() {
 		return lblQuadra;
 	}
 
-
-
 	public void setLblQuadra(JLabel lblQuadra) {
 		this.lblQuadra = lblQuadra;
 	}
-
-
 
 	public JButton getBtnVoltar() {
 		return btnVoltar;
 	}
 
-
-
 	public void setBtnVoltar(JButton btnVoltar) {
 		this.btnVoltar = btnVoltar;
 	}
-
-
 
 	public JButton getBtnDistribuir() {
 		return btnDistribuir;
 	}
 
-
-
 	public void setBtnDistribuir(JButton btnDistribuir) {
 		this.btnDistribuir = btnDistribuir;
 	}
-
-
 
 	public Dimension getSIZE() {
 		return SIZE;
@@ -225,17 +228,16 @@ public class DistribuirJogos extends JFrame {
 		JOptionPane.showMessageDialog(this, "Informe uma Quadra.");
 
 	}
-	
+
 	public void notifyInformarCategoria() {
 		JOptionPane.showMessageDialog(this, "Informe uma Categoria.");
 
 	}
-	
+
 	public void notifySucesso() {
 		JOptionPane.showMessageDialog(this, "Jogos distribuidos com Sucesso!!");
 
 	}
-
 
 	public static void main(String[] args) {
 		Usuario g = new Usuario();
